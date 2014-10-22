84649eecb934ce0f45c0001e96ed3ec9db48c0fd
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-10-22 18:17:36 +0400

    Bump version: 2.0.0 → 3.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='obedient.zookeeper',
-        version='2.0.0',
+        version='3.0.0',
         url='https://github.com/yandex-sysmon/obedient.zookeeper',
         license='LGPLv3',
         author='Nikolay Bryskin',
